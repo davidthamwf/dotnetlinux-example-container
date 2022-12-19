029d7a9cb563ac78d7404be6ac2435fff49983ca
David Tham Wei Fan <davidthamwf@gmail.com>
2022-12-19 11:43:44 +0800

    Update StockViewModel.cs

--- Models/StockViewModel.cs
+++ Models/StockViewModel.cs
@@ -1,15 +1,31 @@
-﻿using System;
+using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 
 namespace AspNetCoreWebApp.Models
 {
+    public class ResponseModel
+    {
+        public Pagination pagination { get; set; }
+        public StockViewModel data = new StockViewModel();
+    }
+    public class Pagination
+    {
+        public int limit { get; set; }
+        public int offset { get; set; }
+        public int count { get; set; }
+        public int total { get; set; }
+    }
+
     public class StockViewModel
     {
-        public string ticker { set; get; }
-        public string dateTime { set; get; }
-        public string close { set; get; }
-        public string volume { set; get; }
+        [JsonProperty(PropertyName = "adj_close")]
+        public string close { get; set; }
+        [JsonProperty(PropertyName = "symbol")]
+        public string ticker { get; set; }
+        [JsonProperty(PropertyName = "date")]
+        public string dateTime { get; set; }
+        [JsonProperty(PropertyName = "adj_volume")]
+        public string volume { get; set; }
     }
-}
